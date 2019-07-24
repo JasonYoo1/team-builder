6695d27371a90b431adb0a64d297c90ed2d1e6c1
JasonYoo1 <jyoojs@gmail.com>
2019-07-24 13:52:35 -0700

    changed to flexible state

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,33 +1,57 @@
 import React, { useState } from 'react';
+import { UserCard } from './UserCard'
 
 export const TeamForm = () => {
-    const [inputData, setInputData] = useState(" ");
-    const submitHandler = (event) => {
-      event.preventDefault()
-      console.log()
+    //setting states for each input
+    const [inputUser, setInputUser] = useState({
+        username: '',
+        email: '',
+        role:''
+    });
+
+
+
+    const submitHandler = (event => {
+        event.preventDefault()
+        console.log(inputUser)
+    })
+
+    const changeHandler = (event) => {
+        console.log(event.target)
     }
+
     return (
         <div className="App">
-        <form >
+        <form onSubmit = {submitHandler}>
           <label>
-            UserName
+            USERNAME
               <input 
               id='username-value'
               type='text'
               placeholder='Enter User'
+                //setInputUser (setState) changes state everytime the value of the enter user text changes.
                 onChange={event => 
-                    {console.log(event.target.value)}
+                    {setInputUser({...inputUser, username: event.target.value})}
                 }
               />
+              EMAIL
               <input
               type='text'
-              placeholder='email'
+              placeholder='Enter Email'
+              onChange={event => 
+                {setInputUser({...inputUser, email: event.target.value})}
+              }
               />
+              ROLE
               <input
               type='text'
-              placeholder='role'
+              placeholder='Enter Role'
+              onChange={event => 
+                {setInputUser({...inputUser, role: event.target.value})}
+              }
               />
           </label>
+          <button>Edit</button>
         </form>
     </div>
     );
